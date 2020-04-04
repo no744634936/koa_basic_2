dde63d457e47b0e051852c8ce360c6d24133ebff
zhanghaifeng2019 <zhang@rentracks.co.jp>
2020-04-04 23:36:26 +0900

    good

--- module/config.js
+++ module/config.js
@@ -1,7 +1,11 @@
-//配置文件
-let db_config={
-    dbUrl:"mongodb://localhost:27017",
-    dbName:"learn_mongo",
-};
 
-module.exports=db_config;
+
+var app={
+
+    dbUrl: 'mongodb://localhost:27017/',
+
+    dbName: 'learn_mongo'
+
+}
+
+module.exports=app;
--- module/db.js
+++ module/db.js
@@ -1,8 +1,8 @@
+
 let MongoClient=require("mongodb").MongoClient;
 let config=require("./config.js");
 
 class Db{
-
 
     constructor(){
         this.connect();
@@ -10,44 +10,30 @@ class Db{
     }
 
     static getInstance=()=>{
-        //如果不存在Db的instance 那么新建一个实例
         if(!(Db.instance)){
             Db.instance=new Db();
         }
         return Db.instance;
-
-        //为什么这样写不行？？？？
-        // if(!Db.instance){
-        //     Db.instance=new Db();
-        // }else{
-        //     return Db.instance;
-        // }
-
-        //为什么这样写也不行？？？？？？
-        // if(Db.instance){
-        //     return Db.instance;
-        // }
-        //  Db.instance=new Db();
-
     }
 
 
     connect=()=>{
-        if(!this.dbClient){
+
             return new Promise((resolve,reject)=>{
-                MongoClient.connect(config.dbUrl,{ useUnifiedTopology:true },(err,client)=>{
-                    if(err){
-                        reject(err);
-                    }else{
-                        let db=client.db(config.dbName);
-                        this.dbClient=db;
-                        resolve(this.dbClient);
-                    }
-                })
+                if(!this.dbClient){
+                    MongoClient.connect(config.dbUrl,{ useUnifiedTopology:true },(err,client)=>{
+                        if(err){
+                            reject(err);
+                        }else{
+                            let db=client.db(config.dbName);
+                            this.dbClient=db;
+                            resolve(this.dbClient);
+                        }
+                    })
+                }else{
+                    resolve(this.dbClient);
+                }
             });
-        }else{
-            resolve(this.dbClient);
-        }
 
     }
 
@@ -78,19 +64,12 @@ class Db{
 
 
 
-//把代码改成单例模式了之后。想多次实例化的时候就不是使用new了。而是是使用静态方法。
-//ctrl 加 alt 加 m 停止运行代码
-
 let test1=Db.getInstance();
-let test2=Db.getInstance();
-let test3=Db.getInstance();
 
 test1.find("test",{}).then((data)=>{
     console.log(data);
 });
-test2.find("test",{}).then((data)=>{
-    console.log(data);
-});
-test3.find("test",{}).then((data)=>{
-    console.log(data);
-});
+
+//test1=Db.getInstance();
+
+module.exports=Db.getInstance();
