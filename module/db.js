2a27c3b9306d15b3fee2f5ee8024f9cf0c303bb0
zhanghaifeng2019 <zhang@rentracks.co.jp>
2020-04-04 19:43:20 +0900

    first commit

--- module/db.js
+++ module/db.js
@@ -4,32 +4,40 @@ let config=require("./config.js");
 class Db{
     constructor(){
         //初始化的时候连接数据库
-        // this.connect();
+        this.connect();
+        this.dbClient=""; //放入db对象
     }
 
-    //连接数据库并返回数据库对象。
+
     connect=()=>{
-        return new Promise((resolve,reject)=>{
-            MongoClient.connect(config.dbUrl,{ useUnifiedTopology:true },(err,client)=>{
-                if(err){
-                    reject(err);
-                }else{
-                    //数据库对象
-                    let db=client.db(config.dbName);
-                    console.log("success");
-                    resolve(db);
-                }
-            })
-        });
+
+        //如果dbClient没有值，那么就把连接的db对象给dbClient
+        //解决数据库多次连接的问题
+        if(!this.dbClient){
+            return new Promise((resolve,reject)=>{
+                MongoClient.connect(config.dbUrl,{ useUnifiedTopology:true },(err,client)=>{
+                    if(err){
+                        reject(err);
+                    }else{
+                        let db=client.db(config.dbName);
+                        this.dbClient=db;
+                        resolve(this.dbClient);
+                    }
+                })
+            });
+        }else{
+            //第二次调用connect方法的时候直接返回
+            resolve(this.dbClient);
+        }
+
     }
 
     find=(collectionName,json)=>{
         return new Promise(async(resolve,reject)=>{
             try{
-                // collectionName 就是数据库里的表单名称，json就是查询条件。
+
                 let db=await this.connect();
                 let result=db.collection(collectionName).find(json);
-                //toArray是mongodb的方法。它必须包含两个参数，第一个是err 第二个是docs
                 result.toArray((err,docs)=>{
                     resolve(docs);
                 })
@@ -37,16 +45,6 @@ class Db{
                 reject(err);
             }
         })
-
-        // return new Promise((resolve,reject)=>{
-        //     this.connect().then((db)=>{
-        //         let result=db.collection(collectionName).find(json);
-        //         toArray是mongodb的方法。
-        //         result.toArray((err,docs)=>{
-        //             resolve(docs);
-        //         })
-        //     })
-        // })
     }
 
     update=async()=>{
@@ -59,8 +57,9 @@ class Db{
 
 }
 
+
+
 let test=new Db();
-//await 只能在async函数里面使用
 test.find("test",{}).then((data)=>{
     console.log(data);
 });
@@ -73,5 +72,9 @@ test.find("test",{}).then((data)=>{
 
 
 
-//这里有个问题
-//问题一: 每次使用find 方法的时候都要连一次数据库。很麻烦
+//这样写就不会每次调用find方法都连接一次数据库了。but这段代码还是有问题。
+//多次实例话的时候还是要，多次连接数据库。所以要使用单例模式。
+
+let test2=new Db();
+
+let test3=new Db();
