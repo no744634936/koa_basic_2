1345c03a978b1e09d2249348daca4703442ad252
zhanghaifeng2019 <zhang@rentracks.co.jp>
2020-04-10 09:22:41 +0900

    good

--- app.js
+++ app.js
@@ -26,15 +26,15 @@ render(app, {
     debug: process.env.NODE_ENV !== 'production'
 });
 
-//配置session
 
+
+
+
+//配置session
 app.keys = ['some secret hurr'];
 
 const CONFIG = {
-    key: 'koa:sess', /** (string) cookie key (default is koa:sess) */
-    /** (number || 'session') maxAge in ms (default is 1 days) */
-    /** 'session' will result in a cookie that expires when session/browser is closed */
-    /** Warning: If a session cookie is stolen, this cookie will never expire */
+    key: 'koa:sess', 
     maxAge: 86400000,
     overwrite: true, /** (boolean) can overwrite or not (default true) */
     httpOnly: true, /** (boolean) httpOnly or not (default true) */
@@ -43,9 +43,6 @@ const CONFIG = {
     renew: false, /** (boolean) renew session when session is nearly expired, so we can always keep user logged in. (default is false)*/
 };
 app.use(session(CONFIG, app));
-
-
-
 
 
 
--- routes/admin.js
+++ routes/admin.js
@@ -12,11 +12,39 @@ var newscate=require('./admin/newscate.js');
 var login=require('./admin/login.js');
 
 
+
+
+
+
+//当用户输入一个包含admin的url的时候（）都会首先经过这个路由
+//让后通过await next(); 像下继续匹配路由
 router.use(async (ctx,next)=>{
 
     ctx.state.__ROOT__='http://'+ctx.header.host;
-    await next();
+
+    // console.log(ctx.session.userInfo);
+    // console.log(ctx.url);
+    
+    //如果有session继续向下匹配路由。
+    if(ctx.session.userInfo){
+        //这个表示继续向下匹配路由，执行代码
+        await next();
+    }else{
+        //如果没有session不存在
+        //如果用户输入的url不是 localhost:3000/admin/login那么跳转到login页面
+        if(ctx.url!="/admin/login" || ctx.url!="/admin/login"){
+            ctx.redirect("/admin/login");
+        }else{
+            // 如果用户输入的url是 localhost:3000/admin/login.继续向下匹配路由。
+            await next();
+        }
+    }
+    
 })
+
+
+
+
 
 //配置admin的子路由  层级路由
 router.get('/',(ctx)=>{
